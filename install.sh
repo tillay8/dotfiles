47283057f6803a100e2dbb407c3dc9dca85bd447
Tilley <tilleyy8@gmail.com>
2024-12-22 18:48:52 -0700

    installer totally works dude

---
+++ install.sh
@@ -0,0 +1,18 @@
+# Some trash i made at 2am to install this that works 100%
+rm -rf ~/.config/alacritty
+cp -r ./alacritty ~/.config
+rm -rf ~/.config/fastfetch
+cp -r ./fastfetch ~/.config
+rm -rf ~/.config/hypr
+cp -r ./hypr ~/.config
+rm -rf ~/.config/kate
+cp -r ./kate ~/.config
+rm -rf ~/.config/macos
+cp -r ./macos ~/.config
+rm -rf ~/.config/waybar
+cp -r ./waybar ~/.config
+rm -rf ~/.config/Vencord
+cp -r ./Vencord ~/.config
+killall -SIGUSR2 waybar
+nohup waybar & 
+nohup hyprpaper & 
