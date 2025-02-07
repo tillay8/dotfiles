17be899dc137512d1e1427d9312aa0582bf001b2
tillay8 <tilleyy8@gmail.com>
2025-02-07 15:31:19 -0700

    add dark mode

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
-sudo pacman -S --noconfirm neovim yay brightnessctl xdotool prismlauncher jdk21-openjdk jdk17-openjdk wget gimp eog xclip gparted docker net-tools macchanger yt-dlp gnome-boxes alacritty figlet python-pip toastify zsh cmus fastfetch gum python-pycryptodome kleopatra konsole kate hyprland waybar hyprpaper vlc mpv xreader thunar xfce4-appfinder zip tree highlight lolcat steam
-yay -S --noconfirm mullvad-vpn-bin discord deluge-gtk ookla-speedtest-bin ani-cli obs-studio gnome-calendar intellij-idea-community-edition edex-ui-bin minecraft-launcher ytfzf
+sudo pacman -Syyu
+sudo pacman -S --noconfirm neovim yay brightnessctl xdotool prismlauncher jdk21-openjdk jdk17-openjdk wget gimp eog xclip gparted docker net-tools macchanger yt-dlp gnome-boxes alacritty figlet python-pip toastify zsh cmus fastfetch gum python-pycryptodome kleopatra konsole kate hyprland waybar hyprpaper vlc mpv xreader thunar xfce4-appfinder zip tree highlight lolcat steam pavucontrol python-cryptography 
+yay -S --noconfirm mullvad-vpn-bin discord deluge-gtk ookla-speedtest-bin ani-cli obs-studio gnome-calendar intellij-idea-community-edition edex-ui-bin minecraft-launcher ytfzf hyprpolkit-agent popsicle xdg-desktop-portal-hyprland
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 curl -sSLf https://github.com/aclap-dev/vdhcoapp/releases/latest/download/install.sh | bash
 sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
+sudo systemctl enable bluetooth
